457f84608afef49255ba8f6887c7f3649b697698
Lucas Martins Domingues <lucas.progmd@gmail.com>
2019-09-18 11:02:22 -0300

    updates

--- dump.sql
+++ dump.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE db_users;
-USE tb_users;
+USE db_users;
 
 CREATE TABLE tb_users(
 	id INT(11) PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(24) NOT NULL,
-    password VARCHAR(8) NOT NULL
+    password VARCHAR(65) NOT NULL
 );
