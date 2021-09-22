76221d1abdc38c5b73442cb7a38e2f844e8e80ab
Lucas M Domingues <lucasmdomingues.dev@gmail.com>
2021-09-21 23:16:54 -0300

    some improvements

--- dump.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE db_users;
-USE db_users;
-
-CREATE TABLE tb_users(
-	id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(24) NOT NULL,
-    password VARCHAR(65) NOT NULL
-);
